f57842c79799502821f1bed535bb48cb14cbd887
sailesh <saileshr2209@gmail.com>
2023-05-14 12:31:45 +0530

    wrapped up the initial state of the parser - create ASTs and report syntax errors

--- src/Lox.java
+++ src/Lox.java
@@ -54,6 +54,14 @@ public class Lox {
         report(line, "", message);
     }
 
+    static void error(Token token, String message) {
+        if (token.type == TokenType.EOF) {
+            report(token.line, " at end", message);
+        } else {
+            report(token.line, " at '" + token.lexeme + "'", message);
+        }
+    }
+
     private static void report(int line, String where, String message) {
         System.err.println("[line " + line + "] Error" + where + ": " + message);
         hadError = true;
