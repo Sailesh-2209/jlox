aab5e49269e39acfc9ab18d9540250a9bee50f9e
sailesh <saileshr2209@gmail.com>
2023-05-17 08:37:36 +0530

    hooked up the interpeter, runtime errors don't break the REPL

--- src/Interpreter.java
+++ src/Interpreter.java
@@ -94,4 +94,27 @@ public class Interpreter implements Expr.Visitor<Object> {
         if (left instanceof Double && right instanceof Double) return;
         throw new RuntimeError(operator, "Operands must be numbers.");
     }
+
+    private String stringify(Object object) {
+        if (object == null) return "nil";
+
+        if (object instanceof Double) {
+            String text = object.toString();
+            if (text.endsWith(".0")) {
+                text = text.substring(0, text.length() - 2);
+                return text;
+            }
+        }
+
+        return object.toString();
+    }
+
+    void interpret(Expr expression) {
+        try {
+            Object value = evaluate(expression);
+            System.out.println(stringify(value));
+        } catch (RuntimeError error) {
+            Lox.runtimeError(error);
+        }
+    }
 }
