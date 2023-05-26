8e5a4b038dc03f615a0670e2a6ac9efebd3bb1be
sailesh <saileshr2209@gmail.com>
2023-05-26 20:16:06 +0530

    implemented for loops

--- src/Parser.java
+++ src/Parser.java
@@ -1,5 +1,5 @@
-import javax.swing.plaf.nimbus.State;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Parser {
@@ -32,6 +32,7 @@ public class Parser {
 
     private Stmt statement() {
         if (match(TokenType.IF)) return ifStatement();
+        if (match(TokenType.FOR)) return forStatement();
         if (match(TokenType.WHILE)) return whileStatement();
         if (match(TokenType.PRINT)) return printStatement();
         if (match(TokenType.LEFT_BRACE)) return new Stmt.Block(block());
@@ -52,6 +53,52 @@ public class Parser {
         }
 
         return new Stmt.If(condition, ifBranch, elseBranch);
+    }
+
+    private Stmt forStatement() {
+        consume(TokenType.LEFT_PAREN, "Expect '(' after 'for'.");
+
+        Stmt initializer = null;
+        if (!match(TokenType.SEMICOLON)) {
+            if (match(TokenType.VAR)) {
+                initializer = variableDeclaration();
+            } else {
+                initializer = expressionStatement();
+            }
+        }
+
+        Expr condition = null;
+        if (!check(TokenType.SEMICOLON)) {
+            condition = expression();
+        }
+        consume(TokenType.SEMICOLON, "Expect ';' after loop condition.");
+
+        Expr increment = null;
+        if (!check(TokenType.RIGHT_PAREN)) {
+            increment = expression();
+        }
+        consume(TokenType.RIGHT_PAREN, "Expect ')' after for clauses.");
+
+        Stmt body = statement();
+
+        if (increment != null) {
+            body = new Stmt.Block(Arrays.asList(
+                    body,
+                    new Stmt.Expression(increment)
+            ));
+        }
+
+        if (condition == null) condition = new Expr.Literal(true);
+        body = new Stmt.While(condition, body);
+
+        if (initializer != null) {
+            body = new Stmt.Block(Arrays.asList(
+                    initializer,
+                    body
+            ));
+        }
+
+        return body;
     }
 
     private Stmt whileStatement() {
